78506974fb82a2af27e41a90e56ce772bf4c3e79
Divesh Agarwal <diveshagarwal1995@gmail.com>
2024-12-23 23:58:39 +0530

    Day - 4

---
+++ If-Else.java
@@ -0,0 +1,12 @@
+# Program to check if a number is positive, negative, or zero
+
+# Input from the user
+number = float(input("Enter a number: "))
+
+# Check the condition
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
